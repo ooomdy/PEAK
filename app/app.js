81e44efca73438dab88f559d6cef63d4240bed1e
Andy Mecke <omdyinc@gmail.com>
2018-06-22 10:25:34 -0400

    Work from yesterday.

---
+++ app/app.js
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+
+
+// document.querySelector('#nominate').innerHTML = "Hello World";
+//
+// document.getElementById('button1').addEventListener('click', function(){
+//   console.log('This button was clicked');
+// });
